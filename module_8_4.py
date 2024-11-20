970694d54b5791a044ba1acbe4002ec224ed0fa5
DjonKrd <dj2393@mail.ru>
2024-11-20 14:30:36 +0300

    Add files via upload

---
+++ module_8_4.py
@@ -0,0 +1,27 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time)/2
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    result = 'Победа команды Волшебники Данных!'
+else:
+    result = 'Ничья!'
+
+
+print('В команде Мастера кода участников: %s!' % '5')
+print('Итого сегодня в командах участников: %(team1_num)s и %(team2_num)s!' % {'team1_num': 5, 'team2_num': 6})
+
+print('Команда Волшебники данных решила задач: {}!'.format(42))
+print('Волшебники данных решили задачи за {} с!'.format(1552.512))
+
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'{result}')
+
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
